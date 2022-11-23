a9cba6eb5002e2aaede67213dc22bee179074188
AlvGD <alvaro.gd@opendeusto.es>
2022-11-23 18:25:32 +0100

    Nombre Proyecto Editado
    Cliente2 --> Cliente
    
    Co-Authored-By: Imanol Duran Tejada <90787303+imanoldt@users.noreply.github.com>

--- Cliente/src/cliente/MainProgram.java
+++ Cliente/src/cliente/MainProgram.java
@@ -10,24 +10,8 @@ public class MainProgram {
 	public static void main(String[] args) {	
 		ServiceLocator serviceLocator = new ServiceLocator();
 		
-		//args[0] = RMIRegistry IP
-		//args[1] = RMIRegistry Port
-		//args[2] = Service Name
 		serviceLocator.setService(args[0], args[1], args[2]);
 		
-		/*
-		LoginController loginController = new LoginController(serviceLocator);
-		LoginDialog loginDialog = new LoginDialog(loginController);			
-		BidController bidController = new BidController(serviceLocator);			
-		BidWindow bidWindow = new BidWindow(bidController);
-		
-		//Login
-		loginDialog.login();		
-		//Get Categories
-		
-		//Logout
-		loginDialog.logout();
-		*/
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
--- Cliente/src/windows/VentanaLoginN.java
+++ Cliente/src/windows/VentanaLoginN.java
@@ -50,20 +50,6 @@ public class VentanaLoginN extends JFrame {
 	/**
 	 * Launch the application.
 	 */
-	/*
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					VentanaLoginN frame = new VentanaLoginN();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-	*/
 
 	/**
 	 * Create the frame.
